df5e1701cad3c264eeb10a9c529d3009eb589f31
Piotr Bieszczad <biecho88@gmail.com>
2024-07-01 13:01:16 +0000

    Added JS to send email.

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,30 @@ menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
 }
+
+function sendMail() {
+    var params = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        number: document.getElementById("number").value,
+        subject: document.getElementById("subject").value,
+        message: document.getElementById("message").value,
+
+    };
+   
+    const serviceID = "service_hapbbxs";
+    const templateID = "template_y7rzenl"
+
+    emailjs
+        .send(serviceID,templateID,params)
+        .then((res) =>{
+                document.getElementById("name").value = "";
+                document.getElementById("email").value = "";
+                document.getElementById("number").value = "";
+                document.getElementById("subject").value = "";
+                document.getElementById("message").value = "";
+                consol.log(res);
+                alert("Your email has been sent. Thanks You")
+        })
+        .catch((err) => consol.log(err));
+}
